aa1ef51f2c220d601bbf918b8f65aad3ec96bc81
liyang <liyang@datapipeline.com>
2022-06-13 21:07:27 +0800

    add

---
+++ java-train/src/main/java/com/datapipeline/locks/LockDemo.java
@@ -0,0 +1,30 @@
+package com.datapipeline.locks;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockDemo {
+
+    // 悲观锁的调用方式
+    public static synchronized void m1(){
+        // 加锁后的逻辑
+    }
+
+    static ReentrantLock lock = new ReentrantLock();
+    public static void m2(){
+        lock.lock();
+        try {
+            // 操作同步资源
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    // 乐观锁的调用方式
+    // 保证多个线程使用的是同一个AtomicInteger
+    public static void m3(){
+        AtomicInteger ato = new AtomicInteger();
+        ato.incrementAndGet();
+    }
+
+}
